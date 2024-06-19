b275427b2807dee46666bbf6f3ad60a1c86e85c3
Christian David Villar Colodro <86432506+ChristianDVillar@users.noreply.github.com>
2024-06-19 18:11:29 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarExcusa() {
+    var who = ["The dog", "My grandma", "The mailman", "My bird"];
+    var action = ["ate", "peed", "crushed", "broke"];
+    var what = ["my homework", "my phone", "the car"];
+    var when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    var persona = who[Math.floor(Math.random() * who.length)];
+    var accion = action[Math.floor(Math.random() * who.length)];
+    var objeto = what[Math.floor(Math.random() * what.length)];
+    var momento = when[Math.floor(Math.random() * when.length)];
+
+    var excuse = `${persona} ${accion} ${objeto} ${momento}.`;
+
+    var textescuse = document.getElementById("excuse");
+    textescuse.innerText = excuse;
+    console.dir(textescuse);
+  }
+  generarExcusa();
+
+  console.log("4 Geeks Academy");
 };
